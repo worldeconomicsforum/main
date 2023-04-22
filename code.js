dd0c95cfe65b8fcf1e6032d3c81c302ab5ff39d3
mattschagas <santos.matheuschagas@gmail.com>
2023-04-22 14:43:28 -0300

    v.0.1.2.x+1

--- code.js
+++ code.js
@@ -1,5 +1,58 @@
 var rellax = new Rellax('.rellax');
 
+document.addEventListener('DOMContentLoaded', function() {
+    function smoothScroll(target, duration) {
+        const targetElement = document.querySelector(target);
+        const targetPosition = targetElement.offsetTop-150;
+        const startPosition = window.pageYOffset;
+        const distance = (targetPosition - startPosition);
+        let startTime = null;
+      
+        function animation(currentTime) {
+          if (startTime === null) startTime = currentTime;
+          const elapsedTime = currentTime - startTime;
+          const run = easing(elapsedTime, startPosition, distance, duration);
+          window.scrollTo(0, run);
+          if (elapsedTime < duration) {
+            requestAnimationFrame(animation);
+          }
+        }
+      
+        function easing(t, b, c, d) {
+          // easing function for smoother animation
+          t /= d / 2;
+          if (t < 1) return c / 2 * t * t + b;
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+      
+        requestAnimationFrame(animation);
+    }
+      
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const target = link.getAttribute('href');
+            const duration = 1000; // adjust the duration value to your preference
+            smoothScroll(target, duration);
+        });
+    });
+
+    const seta = document.getElementById('mv');
+    const setao = seta.querySelector('img');
+
+    seta.addEventListener('click', event => {
+        event.preventDefault();
+        setao.classList.add('rodarodajequiti');
+        setTimeout(() => {
+            setao.classList.remove('rodarodajequiti');
+            window.location.href = seta.href;
+        }, 700);
+    });
+});
+  
+
 window.addEventListener("scroll", function() {
     // Pega a posição vertical do scroll
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -36,3 +89,5 @@ window.addEventListener("scroll", function() {
         });
     }
 });
+
+
